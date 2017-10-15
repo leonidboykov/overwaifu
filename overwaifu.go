299dade20df3214c6c2812d2726771960b7f1169
Leonid Boykov <lordkratos.47@gmail.com>
2017-10-15 03:10:58 +0300

    Fixed typos

--- overwaifu.go
+++ overwaifu.go
@@ -5,6 +5,13 @@ type OverWaifu struct {
 	Characters []Character
 }
 
+// Achievements ...
+type Achievements struct {
+	MostPopularWaifu Character `json:"most_popular_waifu"` // best all score
+	MostSexyWaifu    Character `json:"most_sexy_waifu"`    // best explicit score
+	BestWaifuSkin    Character `json:"best_waifu_skin"`    // the most popular skin
+}
+
 // FetchData ...
 func (ow *OverWaifu) FetchData() {
 	for _, c := range ow.Characters {
