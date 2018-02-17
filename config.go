080fc304764ef8f0705fc46887696d3ea3625cee
Leonid Boykov <lordkratos.47@gmail.com>
2018-02-17 21:42:46 +0300

    Added Heroku metadata

--- config.go
+++ config.go
@@ -1,7 +1,7 @@
 package overwaifu
 
-// Credetials represents credentials for board login
-type Credetials struct {
+// Credentials represents credentials for board login
+type Credentials struct {
 	Login    string `json:"login" env:"OVERWAIFU_LOGIN,required"`
 	Password string `json:"password" env:"OVERWAIFU_PASSWORD,required"`
 }
