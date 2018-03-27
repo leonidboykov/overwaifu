b8caffe597f7e9cbeab753fd99fc19bc5f0b52d4
Leonid Boykov <lordkratos.47@gmail.com>
2018-03-27 20:15:29 +0300

    changed: improved config workflow

--- cmd/overwaifu/main.go
+++ cmd/overwaifu/main.go
@@ -7,43 +7,38 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
 	"time"
 
-	"github.com/caarlos0/env"
 	"github.com/globalsign/mgo"
-	"github.com/joho/godotenv"
 	"github.com/leonidboykov/getmoe"
 	"github.com/leonidboykov/getmoe/board/sankaku"
 
 	"github.com/overwaifu/overwaifu"
+	"github.com/overwaifu/overwaifu/conf"
 )
 
 func main() {
-	// .env file is used for the local development
-	if err := godotenv.Load(); err != nil {
-		fmt.Println(err)
+	config, err := conf.Load("")
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
 	}
 
 	// getCache()
 	// getData()
 	// getLatestDate()
 	// dbJob()
-	testDbVersion()
+	testDbVersion(config)
 }
 
-func testDbVersion() {
-	var mgoConfig overwaifu.MongoDBConfig
-	err := env.Parse(&mgoConfig)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
+func testDbVersion(config *conf.Configuration) {
 	dialInfo := &mgo.DialInfo{
-		Addrs:          mgoConfig.URI,
-		ReplicaSetName: mgoConfig.ReplicaSetName,
-		Username:       mgoConfig.User,
-		Password:       mgoConfig.Password,
-		Source:         mgoConfig.Source,
+		Addrs:          config.DB.URI,
+		ReplicaSetName: config.DB.ReplicaSetName,
+		Username:       config.DB.Username,
+		Password:       config.DB.Password,
+		Source:         config.DB.Source,
 		DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
 			conn, err := tls.Dial("tcp", addr.String(), &tls.Config{})
 			return conn, err
@@ -71,136 +66,130 @@ func testDbVersion() {
 	fmt.Println(string(data))
 }
 
-func getLatestDate() {
-	data, err := ioutil.ReadFile("dest/cache/cache.json")
-	if err != nil {
-		log.Panicln(err)
-	}
+// func getLatestDate() {
+// 	data, err := ioutil.ReadFile("dest/cache/cache.json")
+// 	if err != nil {
+// 		log.Panicln(err)
+// 	}
 
-	var posts []getmoe.Post
-	if err = json.Unmarshal(data, &posts); err != nil {
-		log.Panicln(err)
-	}
+// 	var posts []getmoe.Post
+// 	if err = json.Unmarshal(data, &posts); err != nil {
+// 		log.Panicln(err)
+// 	}
 
-	var latestDate time.Time
-	for _, post := range posts {
-		if post.CreatedAt.After(latestDate) {
-			latestDate = post.CreatedAt
-		}
-	}
+// 	var latestDate time.Time
+// 	for _, post := range posts {
+// 		if post.CreatedAt.After(latestDate) {
+// 			latestDate = post.CreatedAt
+// 		}
+// 	}
 
-	fmt.Println(latestDate.Format("2006-01-02"))
+// 	fmt.Println(latestDate.Format("2006-01-02"))
 
-	var cred overwaifu.SankakuCredentials
-	if err := env.Parse(&cred); err != nil {
-		log.Fatalln(err)
-	}
+// 	var cred overwaifu.SankakuCredentials
+// 	if err := env.Parse(&cred); err != nil {
+// 		log.Fatalln(err)
+// 	}
 
+// 	board := sankaku.ChanSankakuConfig
+// 	board.BuildAuth(cred.User, cred.Password)
+
+// 	board.Query = getmoe.Query{
+// 		Tags: []string{"overwatch", fmt.Sprintf("date:>=%s", latestDate.Format("2006-01-02"))},
+// 		Page: 1,
+// 	}
+
+// 	start := time.Now()
+// 	println("searching for overwatch lewd images")
+// 	newPosts, err := board.RequestAll()
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	println("found", len(newPosts))
+// 	elapsed := time.Since(start)
+// 	log.Printf("OverWaifu runtime %s", elapsed)
+
+// 	for _, newPost := range newPosts {
+// 		found := false
+// 		for _, post := range posts {
+// 			if newPost.Hash == post.Hash {
+// 				found = true
+// 			}
+// 		}
+// 		if !found {
+// 			fmt.Printf("Adding post with hash %s\n", newPost.Hash)
+// 			posts = append(posts, newPost)
+// 		}
+// 	}
+// }
+
+// func dbJob() {
+// 	var mgoConfig overwaifu.MongoDBConfig
+// 	err := env.Parse(&mgoConfig)
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+
+// 	dialInfo := &mgo.DialInfo{
+// 		Addrs:          mgoConfig.URI,
+// 		ReplicaSetName: mgoConfig.ReplicaSetName,
+// 		Username:       mgoConfig.User,
+// 		Password:       mgoConfig.Password,
+// 		Source:         mgoConfig.Source,
+// 		DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
+// 			conn, err := tls.Dial("tcp", addr.String(), &tls.Config{})
+// 			return conn, err
+// 		},
+// 		Timeout: time.Second * 10,
+// 	}
+
+// 	session, err := mgo.DialWithInfo(dialInfo)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	defer session.Close()
+
+// 	data, err := ioutil.ReadFile("dest/cache/cache.json")
+// 	if err != nil {
+// 		log.Panicln(err)
+// 	}
+
+// 	var posts []getmoe.Post
+// 	if err = json.Unmarshal(data, &posts); err != nil {
+// 		log.Panicln(err)
+// 	}
+
+// 	c := session.DB("overwaifu").C("posts")
+// 	for i := range posts {
+// 		fmt.Printf("Pushing %5d of %d\n", i, len(posts))
+// 		if err := c.Insert(&posts[i]); err != nil {
+// 			fmt.Println(err)
+// 		}
+// 	}
+
+// 	// var hashes []string
+// 	// if err := c.Find(nil).Distinct("hash", &hashes); err != nil {
+// 	// 	fmt.Println(err)
+// 	// }
+
+// 	// var ids []int
+// 	// if err := c.Find(nil).Distinct("id", &ids); err != nil {
+// 	// 	fmt.Println(err)
+// 	// }
+
+// 	// count, err := c.Count()
+// 	// if err != nil {
+// 	// 	fmt.Println(err)
+// 	// }
+
+// 	// fmt.Println("posts count", count)
+// 	// fmt.Println("uniq hashes", len(hashes))
+// 	// fmt.Println("uniq ids   ", len(ids))
+// }
+
+func getCache(config *conf.Configuration) {
 	board := sankaku.ChanSankakuConfig
-	board.BuildAuth(cred.User, cred.Password)
-
-	board.Query = getmoe.Query{
-		Tags: []string{"overwatch", fmt.Sprintf("date:>=%s", latestDate.Format("2006-01-02"))},
-		Page: 1,
-	}
-
-	start := time.Now()
-	println("searching for overwatch lewd images")
-	newPosts, err := board.RequestAll()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	println("found", len(newPosts))
-	elapsed := time.Since(start)
-	log.Printf("OverWaifu runtime %s", elapsed)
-
-	for _, newPost := range newPosts {
-		found := false
-		for _, post := range posts {
-			if newPost.Hash == post.Hash {
-				found = true
-			}
-		}
-		if !found {
-			fmt.Printf("Adding post with hash %s\n", newPost.Hash)
-			posts = append(posts, newPost)
-		}
-	}
-}
-
-func dbJob() {
-	var mgoConfig overwaifu.MongoDBConfig
-	err := env.Parse(&mgoConfig)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	dialInfo := &mgo.DialInfo{
-		Addrs:          mgoConfig.URI,
-		ReplicaSetName: mgoConfig.ReplicaSetName,
-		Username:       mgoConfig.User,
-		Password:       mgoConfig.Password,
-		Source:         mgoConfig.Source,
-		DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
-			conn, err := tls.Dial("tcp", addr.String(), &tls.Config{})
-			return conn, err
-		},
-		Timeout: time.Second * 10,
-	}
-
-	session, err := mgo.DialWithInfo(dialInfo)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer session.Close()
-
-	data, err := ioutil.ReadFile("dest/cache/cache.json")
-	if err != nil {
-		log.Panicln(err)
-	}
-
-	var posts []getmoe.Post
-	if err = json.Unmarshal(data, &posts); err != nil {
-		log.Panicln(err)
-	}
-
-	c := session.DB("overwaifu").C("posts")
-	for i := range posts {
-		fmt.Printf("Pushing %5d of %d\n", i, len(posts))
-		if err := c.Insert(&posts[i]); err != nil {
-			fmt.Println(err)
-		}
-	}
-
-	// var hashes []string
-	// if err := c.Find(nil).Distinct("hash", &hashes); err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// var ids []int
-	// if err := c.Find(nil).Distinct("id", &ids); err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// count, err := c.Count()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// fmt.Println("posts count", count)
-	// fmt.Println("uniq hashes", len(hashes))
-	// fmt.Println("uniq ids   ", len(ids))
-}
-
-func getCache() {
-	var cred overwaifu.SankakuCredentials
-	err := env.Parse(&cred)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	board := sankaku.ChanSankakuConfig
-	board.BuildAuth(cred.User, cred.Password)
+	board.BuildAuth(config.SC.Username, config.SC.Password)
 
 	board.Query = getmoe.Query{
 		Tags: []string{"overwatch"},
